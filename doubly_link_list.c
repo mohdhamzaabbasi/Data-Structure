d29ef17436ee0cc719786cdf0669f453062b119c
Mohd Hamza Abbasi <141723368+mohdhamzaabbasi@users.noreply.github.com>
2023-10-21 13:28:22 +0530

    Create doubly_link_list.c

---
+++ doubly_link_list.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
+    }
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
+
+void deleteNode(struct Node** head, int key) {
+    struct Node* current = *head;
+    while (current != NULL && current->data != key) {
+        current = current->next;
+    }
+    if (current == NULL) {
+        printf("Node with data %d not found.\n", key);
+        return;
+    }
+
+    if (current->prev != NULL) {
+        current->prev->next = current->next;
+    } else {
+        *head = current->next;
+    }
+
+    if (current->next != NULL) {
+        current->next->prev = current->prev;
+    }
+
+    free(current);
+}
+
+void displayList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d <-> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtEnd(&head, 3);
+    insertAtBeginning(&head, 0);
+
+    printf("Doubly Linked List: ");
+    displayList(head);
+
+    deleteNode(&head, 2);
+
+    printf("Doubly Linked List after deleting 2: ");
+    displayList(head);
+
+    return 0;
+}
